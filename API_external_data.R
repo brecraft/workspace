f5ce783340927ad0144073f5f77ef4bbb9279aa3
brecraft <33881307+brecraft@users.noreply.github.com>
2018-04-30 10:43:06 -0700

    Add files via upload
    Example of API call to external resource.

---
+++ API_external_data.R
@@ -0,0 +1,22 @@
+library(httr,jsonlite,lubridate)
+
+# API Password: e6a6326f-32e9-490c-a550-0fce0fe89d55
+
+#Define URL and Perform GET
+url  <- "https://holidayapi.com"
+path <- "/v1/holidays?key=e6a6326f-32e9-490c-a550-0fce0fe89d55&country=US&year=2017&month=04"
+raw.result <- GET(url = url, path = path)
+
+#translate content into text
+this.raw.content <- rawToChar(raw.result$content)
+
+#parse the json
+this.content <- fromJSON(this.raw.content)
+
+#convert to dataframe
+this.content.df <- do.call("rbind", this.content)
+
+#Remove the first row which includes the status from the rest call
+this.content.df <- this.content.df[-1,]
+
+
